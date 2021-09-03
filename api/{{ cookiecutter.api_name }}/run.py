80efd97be7300e10fac2d1b3d8b4313685933f69
Afonso Menegola <afonso.menegola@sulamerica.com.br>
2021-09-02 22:33:27 -0300

    api\{{ cookiecutter.api_name }}\run.py

--- api/{{ cookiecutter.api_name }}/run.py
+++ api/{{ cookiecutter.api_name }}/run.py
@@ -1,13 +1,11 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from api.controller import app
-from api.config import get_logger
+from app.controller import app
 import uvicorn
 
 load_dotenv()
 app_env = os.getenv('OS_ENV')
-_logger = get_logger(logger_name=__name__)
 
 if __name__ == '__main__':
     _logger.debug('application instance created')

0cc7ba76d9f3a3df9a3c93c5c9c3ea843359f828
Afonso Menegola <afonso.menegola@sulamerica.com.br>
2021-09-02 23:15:39 -0300

    gcr

--- api/{{ cookiecutter.api_name }}/build_image.sh
+++ api/{{ cookiecutter.api_name }}/build_image.sh
@@ -1,5 +1,5 @@
-docker build -t worshop_image:1.0 .
+docker build -t worshop_image .
 
-docker tag workshop-python gcr.io/static-smoke-324900/workshop-python
+docker tag worshop_image gcr.io/static-smoke-324900/worshop_image
 
-docker push gcr.io/static-smoke-324900/workshop-python:1.0
+docker push gcr.io/static-smoke-324900/worshop_image
